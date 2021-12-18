46a3512f3fc27e6a3ab49959fbf4fe5dfff2a7de
luiscastro8p <luisarmandocastro12@gmail.com>
2021-12-18 15:04:19 -0700

    Se arreglo router

--- challenge_web/src/app/app-routing.module.ts
+++ challenge_web/src/app/app-routing.module.ts
@@ -1,21 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-
+/* PUEDES DECLARAR TODAS LAS RUTAS DESDE AQUI  */
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
   },
-
   {
     path: '',
     loadChildren: () =>
       import('./components/pages/pages.module').then((m) => m.PagesModule),
-  },
-  {
-    path: '**',
-    redirectTo: '/login',
   },
 ];
 
